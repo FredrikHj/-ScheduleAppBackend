ecfc7c826158cf6765189e61bd132a451e690f96
Your Name <you@example.com>
2019-11-04 10:19:41 +0100

    Heroku working

--- src/Components/Data/Axios.js
+++ src/Components/Data/Axios.js
@@ -4,7 +4,7 @@ import { updateSQLDataArr, updateSQLDataColsArr, updateSQLFilterMonthsBtnsArr,  
 let SQLFilterMonthsBtnsArr = [];
 let SQLFilterConcernedBtnsArr = [];
 
-console.log(process.env);
+console.log(process);
 
 let backendURL = `${process.env.baseURL}:${process.env.PORT}` || "http://localhost:5000"; // Heroku
 //let backendURL = 'http://fredrikipnrtelia.mina-projekt.se:3001'; // Pub
