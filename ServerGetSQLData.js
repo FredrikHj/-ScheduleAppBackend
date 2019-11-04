8971e0a78a5c3932a03dd5c80a4f8342d650f507
Your Name <you@example.com>
2019-11-04 14:37:16 +0100

    Deploy testing

--- ServerGetSQLData.js
+++ ServerGetSQLData.js
@@ -7,7 +7,7 @@ var mysql = require('mysql');
 app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 3001;
-console.log(process.env['API_TOKEN']);
+console.log(process.env.PUBLIC_URL);
 
 let currentStatement = '';
 
